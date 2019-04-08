ee6ed356c2757803155e15991f2ce3b37be6f92b
kyriegorrin <kyriegorrin@gmail.com>
2019-04-08 16:40:11 +0100

    test.cpp actualitzat, make clean

--- BaseCompiled/test.cpp
+++ BaseCompiled/test.cpp
@@ -19,8 +19,32 @@ int main(){
 	if(rc != STATUS_OK) std::cout << "Error obrint dispositiu\n\n" << OpenNI::getExtendedError();
 	else std::cout << "Device obert correctament\n\n";
 
+	//Creem video stream de profunditat
+	VideoStream depth;
+
+	if(device.getSensorInfo(SENSOR_DEPTH) != NULL){
+		rc = depth.create(device, SENSOR_DEPTH);
+		if(rc != STATUS_OK) std::cout << "No es pot crear el stream de profunditat\n\n" << OpenNI::getExtendedError();
+		else std::cout << "Stream de profunditat creat\n\n";
+	}
+	
+	//Comencem stream
+	rc = depth.start();
+	if(rc != STATUS_OK) std::cout << "No es pot començar el stream\n\n" << OpenNI::getExtendedError();
+	else std::cout << "Comencem stream\n\n";
+
+
+
 	//Tanquem dispositius i fem shutdown
+	std::cout << "Parant stream\n\n";
+	depth.stop();
+	std::cout << "Eliminant stream object\n\n";
+	depth.destroy();
+	std::cout << "Tancant dispositiu\n\n";
 	device.close();
+	std::cout << "Fent shutdown\n\n";
 	OpenNI::shutdown();
+	std::cout << "FINALITZAT\n\n";
+
 	return 0;
 }
